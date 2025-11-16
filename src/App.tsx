b683b4b722671b14c441ca427ee66b9f6ba27d66
kesavan99 <kesavanbala99@gmail.com>
2025-11-16 13:41:37 +0530

    Remove unused Navigate import

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import SignInForm from './component/SignInForm';
 import SignUpForm from './component/SignUpForm';
 import Home from './component/Home';
